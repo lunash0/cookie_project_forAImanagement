d15b955764b9473b93bab6bb76a4c3da16222c0c
박슬기 <green7170@swu.ac.kr>
2023-08-19 10:03:24 +0000

    cookie 로그인, 회원가입 기능 구현 완료

---
+++ cookie_project/cookie/login.py
@@ -0,0 +1,28 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .models import Accounts
+
+@api_view(['POST'])
+def create_sign_info(request):
+    if request.method == 'POST':
+        user_id = request.data.get('user_id')
+        password = request.data.get('password')
+
+        Accounts.objects.create(USER_ID=user_id, PASSWORD=password)
+
+        return JsonResponse({'message': '회원가입 성공'}, status=201)
+
+@api_view(['POST'])
+def read_login_info(request):
+    if request.method == 'POST':
+        user_id = request.data.get('user_id')
+        password = request.data.get('password')
+
+        search_user = Accounts.objects.filter(USER_ID=user_id, PASSWORD=password)
+
+        if search_user.exists():
+            response_data = {'message': '로그인 성공'}
+        else:
+            response_data = {'message': '로그인 실패'}
+
+        return JsonResponse(response_data)
--- cookie_project/cookie/urls.py
+++ cookie_project/cookie/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include, re_path
 from .models import Accounts
-from . import signup
+from . import login
 
 app_name = 'cookie'
 
 urlpatterns = [
     path('', include('rest_framework.urls', namespace='rest_framework_category')),
-    path('signup/', signup.insert_sign_info, name='insert_sign_info'),
+    path('sign_up/', login.create_sign_info, name='create_sign_info'),
+    path('log_in/', login.read_login_info, name='read_login_info'),
 ]
